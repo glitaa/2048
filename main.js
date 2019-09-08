74a80727e1502513106bb3b373ea3b521d2c1e6d
Adam Glita <adamek161@gmail.com>
2019-09-08 16:07:45 +0200

    Replace old moving code with new

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ var maxLength;
 var fieldsInRow = 4;
 var fieldLength;
 var tiles;
-var leftPressed, rightPressed, upPressed, downPressed;
 var speed = 4;
-var counter = 0;
+var direction = "none";
 var somethingChanged;
+var movedOnce, merged, movedTwice;
 var backgroundColor = "#fff2e6";
 var boardLength;
+var tilesAreInMove;
 
 window.onload = function() {
 	canvasSetup();
@@ -38,147 +39,198 @@ function update() {
 
 function moveEverything() {
 	updateTileSpeed();
+	if (!movedOnce) {
+		moveTiles();
 
-	if (leftPressed) {
-		for (var i = 0; i<fieldsInRow; i++) {
-		for (var j = 0; j<fieldsInRow; j++) {
-			if (tiles[i][j].getValue() > 0) {
-				if (i-1 < 0) //there's a wall on the left
-					break;
-				if (tiles[i-1][j].getValue() > 0) { //there's a tile on the left
-					if (tiles[i-1][j].getValue() !== tiles[i][j].getValue()) //the tile on the left has got other value
-						break;
-					if (tiles[i-1][j].getValue() == tiles[i][j].getValue()) { //the tile on the left has got the same value (merging)
+		areTilesInMove();
+
+		if (!tilesAreInMove && direction != "none")
+			movedOnce = true;
+	}
+
+	if (movedOnce && !merged) {
+		mergeTiles();
+
+		areTilesInMove();
+
+		if (!tilesAreInMove && direction != "none")
+			merged = true;
+	}
+
+	if (movedOnce && merged && !movedTwice) {
+		moveTiles();
+
+		areTilesInMove();
+
+		if (!tilesAreInMove && direction != "none")
+			movedTwice = true;
+	}
+
+	if (movedTwice) {
+		if (somethingChanged) {
+			drawNewTile(1);
+			somethingChanged = false;
+		}
+		direction = "none";
+		movedOnce = merged = movedTwice = false;
+	}
+}
+
+function moveTiles() {
+	switch (direction) {
+		case "left":
+			for (var j = 0; j < fieldsInRow; j++) {
+				for (var i = 1; i < fieldsInRow; i++) {
+					if (tiles[i][j].getValue() > 0) {
+						if (tiles[i - 1][j].getValue() > 0 && tiles[i - 1][j].getValue() != tiles[i][j].getValue())
+							continue;
+						else if (tiles[i - 1][j].getValue() == 0) {
+							if (tiles[i][j].getTempX() == 0)
+								tiles[i][j].setTempX(margin + innerMargin + tiles[i][j].getX() * fieldLength);
+							tiles[i][j].setXSpeed(tiles[i][j].getXSpeed() - speed);
+							if (margin + innerMargin + tiles[i][j].getX() * fieldLength + tiles[i][j].getXSpeed() <= tiles[i][j].getTempX() - fieldLength) {
+								tiles[i][j].move(i - 1, j);
+								if (i - 2 < 0)
+									continue;
+								else if (tiles[i - 2][j].getValue() == 0)
+									tiles[i - 1][j].setXSpeed(tiles[i - 1][j].getXSpeed() - speed);
+							}
+						}
+					}
+				}
+			}
+			break;
+		case "up":
+			for (var j = 0; j < fieldsInRow; j++) {
+				for (var i = 1; i < fieldsInRow; i++) {
+					if (tiles[j][i].getValue() > 0) {
+						if (tiles[j][i - 1].getValue() > 0 && tiles[j][i - 1].getValue() != tiles[j][i].getValue())
+							continue;
+						else if (tiles[j][i - 1].getValue() == 0) {
+							if (tiles[j][i].getTempY() == 0)
+								tiles[j][i].setTempY(margin + innerMargin + tiles[j][i].getY() * fieldLength);
+							tiles[j][i].setYSpeed(tiles[j][i].getYSpeed() - speed);
+							if (margin + innerMargin + tiles[j][i].getY() * fieldLength + tiles[j][i].getYSpeed() <= tiles[j][i].getTempY() - fieldLength) {
+								tiles[j][i].move(j, i - 1);
+								if (i - 2 < 0)
+									continue;
+								else if (tiles[j][i - 2].getValue() == 0)
+									tiles[j][i - 1].setYSpeed(tiles[j][i - 1].getYSpeed() - speed);
+							}
+						}
+					}
+				}
+			}
+			break;
+		case "right":
+			for (var j = 0; j < fieldsInRow; j++) {
+				for (var i = fieldsInRow - 2; i > -1; i--) {
+					if (tiles[i][j].getValue() > 0) {
+						if (tiles[i + 1][j].getValue() > 0 && tiles[i + 1][j].getValue() != tiles[i][j].getValue())
+							continue;
+						else if (tiles[i + 1][j].getValue() == 0) {
+							if (tiles[i][j].getTempX() == 0)
+								tiles[i][j].setTempX(margin + innerMargin + tiles[i][j].getX() * fieldLength);
+							tiles[i][j].setXSpeed(tiles[i][j].getXSpeed() + speed);
+							if (margin + innerMargin + tiles[i][j].getX() * fieldLength + tiles[i][j].getXSpeed() >= tiles[i][j].getTempX() + fieldLength) {
+								tiles[i][j].move(i + 1, j);
+								if (i + 2 > fieldsInRow - 1)
+									continue;
+								else if (tiles[i + 2][j].getValue() == 0)
+									tiles[i + 1][j].setXSpeed(tiles[i + 1][j].getXSpeed() + speed);
+							}
+						}
+					}
+				}
+			}
+			break;
+		case "down":
+			for (var j = 0; j < fieldsInRow; j++) {
+				for (var i = fieldsInRow - 2; i > -1; i--) {
+					if (tiles[j][i].getValue() > 0) {
+						if (tiles[j][i + 1].getValue() > 0 && tiles[j][i + 1].getValue() != tiles[j][i].getValue())
+							continue;
+						else if (tiles[j][i + 1].getValue() == 0) {
+							if (tiles[j][i].getTempY() == 0)
+								tiles[j][i].setTempY(margin + innerMargin + tiles[j][i].getY() * fieldLength);
+							tiles[j][i].setYSpeed(tiles[j][i].getYSpeed() + speed);
+							if (margin + innerMargin + tiles[j][i].getY() * fieldLength + tiles[j][i].getYSpeed() >= tiles[j][i].getTempY() + fieldLength) {
+								tiles[j][i].move(j, i + 1);
+								if (i + 2 > fieldsInRow - 1)
+									continue;
+								else if (tiles[j][i + 2].getValue() == 0)
+									tiles[j][i + 1].setYSpeed(tiles[j][i + 1].getYSpeed() + speed);
+							}
+						}
+					}
+				}
+			}
+			break;
+	}
+}
+
+function mergeTiles() {
+	switch (direction) {
+		case "left":
+			for (var j = 0; j < fieldsInRow; j++) {
+				for (var i = 1; i < fieldsInRow; i++) {
+					if (tiles[i][j].getValue() > 0 && tiles[i - 1][j].getValue() == tiles[i][j].getValue()) {
 						if (tiles[i][j].getTempX() == 0)
-							tiles[i][j].setTempX(margin+innerMargin+tiles[i][j].getX()*fieldLength);
-						tiles[i][j].setXSpeed(tiles[i][j].getXSpeed()-fieldLength/speed);
-						if (margin+innerMargin+tiles[i][j].getX()*fieldLength+tiles[i][j].getXSpeed()<=tiles[i][j].getTempX()-fieldLength) {
-							tiles[i-1][j].setValue(tiles[i-1][j].getValue()*2);
-							tiles[i][j].clear();
-							somethingChanged = true;
+							tiles[i][j].setTempX(margin + innerMargin + tiles[i][j].getX() * fieldLength);
+						tiles[i][j].setXSpeed(tiles[i][j].getXSpeed() - speed);
+						if (margin + innerMargin + tiles[i][j].getX() * fieldLength + tiles[i][j].getXSpeed() <= tiles[i][j].getTempX() - fieldLength) {
+							tiles[i][j].merge(tiles[i - 1][j]);
 						}
-					}
-				} else { //there isn't a tile on the left
-					if (tiles[i][j].getTempX() == 0)
-						tiles[i][j].setTempX(margin+innerMargin+tiles[i][j].getX()*fieldLength);
-					tiles[i][j].setXSpeed(tiles[i][j].getXSpeed()-fieldLength/speed);
-					if (margin+innerMargin+tiles[i][j].getX()*fieldLength+tiles[i][j].getXSpeed()<=tiles[i][j].getTempX()-fieldLength) {
-						tiles[i-1][j].setValue(tiles[i][j].getValue());
-						tiles[i-1][j].setX(tiles[i][j].getX()-1);
-						tiles[i-1][j].setY(tiles[i][j].getY());
-						tiles[i][j].clear();
-						somethingChanged = true;
+						i++;
 					}
 				}
 			}
-		}}
-
-		leftPressed = callCounter();
-	} else if (rightPressed) {
-		for (var i = fieldsInRow-1; i>-1; i--) {
-		for (var j = 0; j<fieldsInRow; j++) {
-			if (tiles[i][j].getValue() > 0) {
-				if (i+1 > fieldsInRow-1) //there's a wall on the right
-					break;
-				if (tiles[i+1][j].getValue() > 0) { //there's a tile on the right
-					if (tiles[i+1][j].getValue() !== tiles[i][j].getValue()) //the tile on the right has got other value
-						break;
-					if (tiles[i+1][j].getValue() == tiles[i][j].getValue()) { //the tile on the right has got the same value (merging)
+			break;
+		case "up":
+			for (var j = 0; j < fieldsInRow; j++) {
+				for (var i = 1; i < fieldsInRow; i++) {
+					if (tiles[j][i].getValue() > 0 && tiles[j][i - 1].getValue() == tiles[j][i].getValue()) {
+						if (tiles[j][i].getTempY() == 0)
+							tiles[j][i].setTempY(margin + innerMargin + tiles[j][i].getY() * fieldLength);
+						tiles[j][i].setYSpeed(tiles[j][i].getYSpeed() - speed);
+						if (margin + innerMargin + tiles[j][i].getY() * fieldLength + tiles[j][i].getYSpeed() <= tiles[j][i].getTempY() - fieldLength) {
+							tiles[j][i].merge(tiles[j][i - 1]);
+						}
+						i++;
+					}
+				}
+			}
+			break;
+		case "right":
+			for (var j = 0; j < fieldsInRow; j++) {
+				for (var i = fieldsInRow - 2; i > -1; i--) {
+					if (tiles[i][j].getValue() > 0 && tiles[i + 1][j].getValue() == tiles[i][j].getValue()) {
 						if (tiles[i][j].getTempX() == 0)
-							tiles[i][j].setTempX(margin+innerMargin+tiles[i][j].getX()*fieldLength);
-						tiles[i][j].setXSpeed(tiles[i][j].getXSpeed()+fieldLength/speed);
-						if (margin+innerMargin+tiles[i][j].getX()*fieldLength+tiles[i][j].getXSpeed()>=tiles[i][j].getTempX()+fieldLength) {
-							tiles[i+1][j].setValue(tiles[i+1][j].getValue()*2);
-							tiles[i][j].clear();
-							somethingChanged = true;
+							tiles[i][j].setTempX(margin + innerMargin + tiles[i][j].getX() * fieldLength);
+						tiles[i][j].setXSpeed(tiles[i][j].getXSpeed() + speed);
+						if (margin + innerMargin + tiles[i][j].getX() * fieldLength + tiles[i][j].getXSpeed() >= tiles[i][j].getTempX() + fieldLength) {
+							tiles[i][j].merge(tiles[i + 1][j]);
 						}
-					}
-				} else { //there isn't a tile on the right
-					if (tiles[i][j].getTempX() == 0)
-						tiles[i][j].setTempX(margin+innerMargin+tiles[i][j].getX()*fieldLength);
-					tiles[i][j].setXSpeed(tiles[i][j].getXSpeed()+fieldLength/speed);
-					if (margin+innerMargin+tiles[i][j].getX()*fieldLength+tiles[i][j].getXSpeed()>=tiles[i][j].getTempX()+fieldLength) {
-						tiles[i+1][j].setValue(tiles[i][j].getValue());
-						tiles[i+1][j].setX(tiles[i][j].getX()+1);
-						tiles[i+1][j].setY(tiles[i][j].getY());
-						tiles[i][j].clear();
-						somethingChanged = true;
+						i--;
 					}
 				}
 			}
-		}}
-
-		rightPressed = callCounter();
-	} else if (upPressed) {
-		for (var i = 0; i<fieldsInRow; i++) {
-		for (var j = 0; j<fieldsInRow; j++) {
-			if (tiles[j][i].getValue() > 0) {
-				if (i-1 < 0) //there's a wall on the top
-					break;
-				if (tiles[j][i-1].getValue() > 0) { //there's a tile on the top
-					if (tiles[j][i-1].getValue() !== tiles[j][i].getValue()) //the tile on the top has got other value
-						break;
-					if (tiles[j][i-1].getValue() == tiles[j][i].getValue()) { //the tile on the top has got the same value (merging)
+			break;
+		case "down":
+			for (var j = 0; j < fieldsInRow; j++) {
+				for (var i = fieldsInRow - 2; i > -1; i--) {
+					if (tiles[j][i].getValue() > 0 && tiles[j][i + 1].getValue() == tiles[j][i].getValue()) {
 						if (tiles[j][i].getTempY() == 0)
-							tiles[j][i].setTempY(margin+innerMargin+tiles[j][i].getY()*fieldLength);
-						tiles[j][i].setYSpeed(tiles[j][i].getYSpeed()-fieldLength/speed);
-						if (margin+innerMargin+tiles[j][i].getY()*fieldLength+tiles[j][i].getYSpeed()<=tiles[j][i].getTempY()-fieldLength) {
-							tiles[j][i-1].setValue(tiles[j][i-1].getValue()*2);
-							tiles[j][i].clear();
-							somethingChanged = true;
+							tiles[j][i].setTempY(margin + innerMargin + tiles[j][i].getY() * fieldLength);
+						tiles[j][i].setYSpeed(tiles[j][i].getYSpeed() + speed);
+						if (margin + innerMargin + tiles[j][i].getY() * fieldLength + tiles[j][i].getYSpeed() >= tiles[j][i].getTempY() + fieldLength) {
+							tiles[j][i].merge(tiles[j][i + 1]);
 						}
-					}
-				} else { //there isn't a tile on the top
-					if (tiles[j][i].getTempY() == 0)
-						tiles[j][i].setTempY(margin+innerMargin+tiles[j][i].getY()*fieldLength);
-					tiles[j][i].setYSpeed(tiles[j][i].getYSpeed()-fieldLength/speed);
-					if (margin+innerMargin+tiles[j][i].getY()*fieldLength+tiles[j][i].getYSpeed()<=tiles[j][i].getTempY()-fieldLength) {
-						tiles[j][i-1].setValue(tiles[j][i].getValue());
-						tiles[j][i-1].setX(tiles[j][i].getX());
-						tiles[j][i-1].setY(tiles[j][i].getY()-1);
-						tiles[j][i].clear();
-						somethingChanged = true;
+						i--;
 					}
 				}
 			}
-		}}
-
-		upPressed = callCounter();
-	} else if (downPressed) {
-		for (var i = fieldsInRow-1; i>-1; i--) {
-		for (var j = 0; j<fieldsInRow; j++) {
-			if (tiles[j][i].getValue() > 0) {
-				if (i+1 > fieldsInRow-1) //there's a wall on the bottom
-					break;
-				if (tiles[j][i+1].getValue() > 0) { //there's a tile on the bottom
-					if (tiles[j][i+1].getValue() !== tiles[j][i].getValue()) //the tile on the bottom has got other value
-						break;
-					if (tiles[j][i+1].getValue() == tiles[j][i].getValue()) { //the tile on the bottom has got the same value (merging)
-						if (tiles[j][i].getTempY() == 0)
-							tiles[j][i].setTempY(margin+innerMargin+tiles[j][i].getY()*fieldLength);
-						tiles[j][i].setYSpeed(tiles[j][i].getYSpeed()+fieldLength/speed);
-						if (margin+innerMargin+tiles[j][i].getY()*fieldLength+tiles[j][i].getYSpeed()>=tiles[j][i].getTempY()+fieldLength) {
-							tiles[j][i+1].setValue(tiles[j][i+1].getValue()*2);
-							tiles[j][i].clear();
-							somethingChanged = true;
-						}
-					}
-				} else { //there isn't a tile on the bottom
-					if (tiles[j][i].getTempY() == 0)
-						tiles[j][i].setTempY(margin+innerMargin+tiles[j][i].getY()*fieldLength);
-					tiles[j][i].setYSpeed(tiles[j][i].getYSpeed()+fieldLength/speed);
-					if (margin+innerMargin+tiles[j][i].getY()*fieldLength+tiles[j][i].getYSpeed()>=tiles[j][i].getTempY()+fieldLength) {
-						tiles[j][i+1].setValue(tiles[j][i].getValue());
-						tiles[j][i+1].setX(tiles[j][i].getX());
-						tiles[j][i+1].setY(tiles[j][i].getY()+1);
-						tiles[j][i].clear();
-						somethingChanged = true;
-					}
-				}
-			}
-		}}
-
-		downPressed = callCounter();
+			break;
 	}
 }
 
@@ -190,6 +242,17 @@ function drawEverything() {
 	drawFields();
 
 	drawTiles();
+}
+function areTilesInMove() {
+	tilesAreInMove = false;
+	for (var i = 0; i < fieldsInRow; i++) {
+		for (var j = 0; j < fieldsInRow; j++) {
+			if (tiles[i][j].getXSpeed() != 0 || tiles[i][j].getYSpeed() != 0) {
+				tilesAreInMove = true;
+				return;
+			}
+		}
+	}
 }
 
 function drawNewTile(numberOfTiles) {
@@ -249,18 +312,8 @@ function addToYSpeedIfIsPositive() {
 	}
 }
 
-function callCounter() {
-	if (counter < speed*fieldsInRow) {
-		if (counter + 1 == speed*fieldsInRow) {
-			counter = 0;
-			if (somethingChanged) {
-				drawNewTile(1);
-				somethingChanged = false;
 			}
-			return false;
 		}
-		counter++;
-		return true;
 	}
 }
 
@@ -328,28 +381,22 @@ function drawTiles() {
 }
 
 document.addEventListener('keydown', function(event) {
-	if (event.code == 'ArrowLeft') {
-		if (!rightPressed && !upPressed && !downPressed) {
-			somethingChanged = false;
-			leftPressed = true;
-		}
-	}
-	if (event.code == 'ArrowRight') {
-		if (!leftPressed && !upPressed && !downPressed) {
-			somethingChanged = false;
-			rightPressed = true;
-		}
-	}
-	if (event.code == 'ArrowUp') {
-		if (!leftPressed && !rightPressed && !downPressed) {
-			somethingChanged = false;
-			upPressed = true;
-		}
-	}
-	if (event.code == 'ArrowDown') {
-		if (!leftPressed && !rightPressed && !upPressed) {
-			somethingChanged = false;
-			downPressed = true;
-		}
+	switch (event.code) {
+		case 'ArrowLeft':
+			if (direction == "none") {
+				direction = "left";
+			} break;
+		case 'ArrowRight':
+			if (direction == "none") {
+				direction = "right";
+			} break;
+		case 'ArrowUp':
+			if (direction == "none") {
+				direction = "up";
+			} break;
+		case 'ArrowDown':
+			if (direction == "none") {
+				direction = "down";
+			}
 	}
 });
